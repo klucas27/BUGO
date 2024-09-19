d975859a0145a0a9eb75264fd6081f9e99850182
kresley <kresleylucas.r@gmail.com>
2024-09-19 16:54:15 -0300

    corrompid

--- main/main2.py
+++ main/main2.py
@@ -1,7 +1,7 @@
 import openpyxl.workbook
 import pandas as pd
 import openpyxl
-
+import os
 
 class Main2:
     def __init__(self) -> None:
@@ -12,43 +12,73 @@ class Main2:
         sheet_produtos = workbook_new["PRUDUTOS"]
         sheet_vendas = workbook_new["VENDAS"]
         df = pd.read_excel("RELATORIO_VENDAS.xlsx", sheet_name="PRUDUTOS")
+        df_vendas = pd.read_excel("RELATORIO_VENDAS.xlsx", sheet_name="VENDAS")
+    
         linha_inserir = sheet_produtos.max_row + 1
         linha_inserir_vendas = sheet_vendas.max_row + 1    
-        
+        # print(produto[2])
         #print(sheet_produtos.max_row)
-        if produto[0] in df.iloc[:, 2].values:
+        if produto[2] == "":
+                produto[2] = "S/V"
+        if produto[2] in df.iloc[:, 3].values and produto[1] in df.iloc[:, 2].values:
+            pass
+        elif produto[1] in df.iloc[:, 2].values and produto[2] == "":
             pass
         else:
-            sheet_produtos[f"C{linha_inserir}"] = f"{produto[0]}"
-            sheet_produtos[f"B{linha_inserir}"] = f"{produto[1]}"
+            sheet_produtos[f"B{linha_inserir}"] = f"{produto[0]}"   #  SKU
+            sheet_produtos[f"C{linha_inserir}"] = f"{produto[1]}"   # Nome
+            sheet_produtos[f"D{linha_inserir}"] = f"{produto[2]}"   # variação
             custo = input("informe o valor do custo do produto: ")
-            sheet_produtos[f"D{linha_inserir}"] = custo
+            sheet_produtos[f"E{linha_inserir}"] = custo
+        
+        # SAVE PRODUTOS
         workbook_new.save("RELATORIO_VENDAS.xlsx")
         
         
-        
-        sheet_vendas[f"B{linha_inserir_vendas}"] = f"{id_pedido}"  # ID
-        sheet_vendas[f"C{linha_inserir_vendas}"] = f"{produto[1]}"  # SKU
-        sheet_vendas[f"D{linha_inserir_vendas}"] = f"{produto[0]}"  #  Nome
-        sheet_vendas[f"E{linha_inserir_vendas}"] = f"{produto[2]}"  # Data PEdido
-        sheet_vendas[f"F{linha_inserir_vendas}"] = f"{produto[3]}"  # Valor Pedido
-        
-        valor_pago_plataforma = float(produto[7]) + float(produto[8]) + float(produto[9]) + float(produto[10]) + float(produto[6])
-        sheet_vendas[f"G{linha_inserir_vendas}"] = f"{valor_pago_plataforma}"  #Valor Pago Plataforma
-        custo_produto = 0
-        line_find = 0
-        #print(sheet_vendas["C"][3].value)
-        for linha in sheet_produtos['C']:
-            # print(linha.value)
-            if linha.value == produto[0]:
-                print(linha.value)
-                custo_produto = sheet_produtos[f"D{line_find+1}"].value
-            line_find +=1
+        if id_pedido in df_vendas.iloc[:, 1].values and produto[1] in df_vendas.iloc[:, 3].values and produto[2] in df_vendas.iloc[:, 4].values:
+            pass
+        elif id_pedido in df_vendas.iloc[:, 1].values and produto[1] in df_vendas.iloc[:, 3].values and produto[2] == "":
+            pass
+        else:
+            sheet_vendas[f"B{linha_inserir_vendas}"] = f"{id_pedido}"   #  ID
+            sheet_vendas[f"C{linha_inserir_vendas}"] = f"{produto[0]}"  #  SKU
+            sheet_vendas[f"D{linha_inserir_vendas}"] = f"{produto[1]}"  #  Nome
+            sheet_vendas[f"E{linha_inserir_vendas}"] = f"{produto[2]}"  #  Variação
+            sheet_vendas[f"F{linha_inserir_vendas}"] = f"{produto[3]}"  #  Quantidade produto
+            sheet_vendas[f"G{linha_inserir_vendas}"] = f"{produto[4]}"  #  Estado do cliente
+            sheet_vendas[f"H{linha_inserir_vendas}"] = f"{produto[5]}"  #  DATA DO PEDIDO
+            sheet_vendas[f"I{linha_inserir_vendas}"] = f"{produto[6]}"  #  VALOR DO PEDIDO
             
-        print(custo_produto)
-        sheet_vendas[f"H{linha_inserir_vendas}"] = f"{custo_produto}"  # Custo Produto
-       # sheet_vendas[f"I{linha_inserir_vendas}"] = f"{produto[0]}"  #Lucro final
+            custo_plataforma = float(produto[6]) - (float(produto[7]) + float(produto[8]) + float(produto[9]) + float(produto[10]) + float(produto[11]))
+            
+            sheet_vendas[f"J{linha_inserir_vendas}"] = custo_plataforma  #  VALOR PAGO A PLATAFORMA
+            
+            custo_produto = 0
+            line_find = 0
+            #print(sheet_vendas["C"][3].value)
+            for linha in sheet_produtos['C']:
+                #print(linha.value)                
+                if linha.value == produto[1] and f"{sheet_produtos[f"D{line_find+1}"].value}" == f"{produto[2]}":
+                    # if linha.value == "S/V"
+                    # # print(linha.value)
+                    custo_produto = float(sheet_produtos[f"E{line_find+1}"].value)
+                line_find +=1
+            
+            sheet_vendas[f"K{linha_inserir_vendas}"] = custo_produto*float(produto[3]) #  VALOR PAGO A PLATAFORMA
+
+            # lucro_final = 
+            
+        #SAVE TUDO
         workbook_new.save("RELATORIO_VENDAS.xlsx")
+        
+        
+        
+        
+            
+    #     print(custo_produto)
+    #     sheet_vendas[f"H{linha_inserir_vendas}"] = f"{custo_produto}"  # Custo Produto
+    #    # sheet_vendas[f"I{linha_inserir_vendas}"] = f"{produto[0]}"  #Lucro final
+    #     workbook_new.save("RELATORIO_VENDAS.xlsx")
     
     def save_pedidos(linha_inserir, infos_pedidos):
         pass
@@ -77,24 +107,25 @@ class Main2:
                 linha_achada = 1
                 for linha in pnl_all["A"]:
                     if linha.value == pnl[f"D{cont}"].value:
-                        print(pnl_all[f"M{linha_achada}"].value)
-                        Main2.save_produtos([pnl_all[f"M{linha_achada}"].value,  #  0 Nome Produto
-                                            pnl_all[f"N{linha_achada}"].value,  #  1 SKU Produto
-                                            pnl_all[f"K{linha_achada}"].value,  #  2 data e hora pagamento (pedido caiu para embalar)
-                                            pnl_all[f"Q{linha_achada}"].value,  #  3 Peço de venda
-                                            pnl_all[f"R{linha_achada}"].value,  #  4 Quantidade
-                                            pnl_all[f"T{linha_achada}"].value,  #  5 SubTotal  
-                                            pnl_all[f"AB{linha_achada}"].value,  #  6 Cupom Vendedor  
-                                            pnl_all[f"AC{linha_achada}"].value,  #  7 Coin Cashback  
-                                            pnl_all[f"AF{linha_achada}"].value,  #  8 Desconto + por -  
-                                            pnl_all[f"AO{linha_achada}"].value,  #  9 Taxa comissao  
-                                            pnl_all[f"AP{linha_achada}"].value,  #  10 Taxa Serviço  
-                                            ], pnl_all[f"A{linha_achada}"].value)
+                        print(f"-> PRODUTO: {pnl_all[f"M{linha_achada}"].value}\n  -> VARIAÇÃO: {pnl_all[f"O{linha_achada}"].value}\n  -> SKU: {pnl_all[f"N{linha_achada}"].value}")
+                        Main2.save_produtos([pnl_all[f"N{linha_achada}"].value,  #  0 SKU PRODUTO
+                                            pnl_all[f"M{linha_achada}"].value,   #  1 NOME PRODUTO
+                                            pnl_all[f"O{linha_achada}"].value,   #  2 VARIAÇÃO PRODUTO
+                                            pnl_all[f"R{linha_achada}"].value,   #  3 QUANTIDADE
+                                            pnl_all[f"BA{linha_achada}"].value,  #  4 ESTADO DO CLIENTE
+                                            pnl_all[f"K{linha_achada}"].value,   #  5 DATA E HORA QUE O PEDIDO FOI LIBERADO
+                                            pnl_all[f"T{linha_achada}"].value,   #  6 SUBTOTAL 
+                                            pnl_all[f"AB{linha_achada}"].value,  #  7 CUPOM VENDEDOR  
+                                            pnl_all[f"AC{linha_achada}"].value,  #  8 COIN CASHBACK  
+                                            pnl_all[f"AG{linha_achada}"].value,  #  9 DESCONTO + por -  
+                                            pnl_all[f"AO{linha_achada}"].value,  #  10 TAXA DE COMISSAO  
+                                            pnl_all[f"AP{linha_achada}"].value,  #  11 TAXA DE SERVIÇO
+                                            ], pnl_all[f"A{linha_achada}"].value,)
                     linha_achada += 1
                 
                   
             
-                
+                os.system('cls')
             elif f"{pnl[f"C{cont}"].value}" == "Saque" and ctt == 0:
                 total_valor -= float(pnl[f"H{cont}"].value)   
                 
